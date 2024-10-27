7cc0a52ee7ee36783003cb45cea63e0cf56d1a64
unknown <smarquez@servicios.mc4.com.bo>
2024-10-27 19:12:07 -0400

    servicios

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../modelos/user';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private host: String = "http://localhost:8081";
 
   constructor(private http: HttpClient) { }
 
-  getUser () {
-    return this.http.get(this.host+"users");
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(environment.api + environment.backend.user + `/all`);
   }
 }
