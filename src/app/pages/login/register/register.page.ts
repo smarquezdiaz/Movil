7864b684aed38f828d73d20cb61cde7781ca6b23
smarquezdiaz <solabril2015@hotmail.com>
2024-11-08 13:33:09 -0400

    arreglos de seleccion de usuario

--- src/app/pages/login/register/register.page.ts
+++ src/app/pages/login/register/register.page.ts
@@ -7,24 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  selectedUser: string | null = null;
-
-
   constructor(private router: Router) { }
-
   ngOnInit() {
-  }
-
-  selectUser(userType: string) {
-    this.selectedUser = userType;
-  }
-
-  continue() {
-    if (this.selectedUser) {
-      console.log('Usuario seleccionado:', this.selectedUser);
-    } else {
-      console.log('Por favor, selecciona un tipo de usuario');
-    }
   }
 
 }
