0f13c136eda098ba7dac17e6795586eeabaeacd4
jhosyS <52686073+jhosyS@users.noreply.github.com>
2024-11-29 18:22:42 -0400

    Actualizar perfil-empresa.page.ts

--- src/app/pages/Perfil-empresa/perfil-empresa.page.ts
+++ src/app/pages/Perfil-empresa/perfil-empresa.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmpresaService } from '../../services/empresa.service';
-import { Empresa } from '../../models/empresa.model';
+import { Empresa } from '../../models/empresa';
 import { ActivatedRoute, Router } from '@angular/router'; 
 
 @Component({
