8c75b76ed0f6da5ada46bb87b10eaddc826322ca
jhosyS <52686073+jhosyS@users.noreply.github.com>
2024-11-30 14:18:24 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'http://localhost:8080',  
+  backend: {
+    user: '/user',             
+    empresa: '/empresas',        
+    convocatoria: '/convocatorias',  
+    imagen: '/imagen',           
+    postulante: '/postulantes'   
+  }
 };
