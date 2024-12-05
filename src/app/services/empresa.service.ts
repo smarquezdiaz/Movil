3d41dabd9b4b77b61ba14828488b4a5836c9a65e
jhosyS <52686073+jhosyS@users.noreply.github.com>
2024-12-05 01:10:46 -0400

    Update empresa.service.ts

--- src/app/services/empresa.service.ts
+++ src/app/services/empresa.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EmpresaDTO } from '../models/empresa.dto';
+import { ConvocatoriaForTableDTO } from '../models/convocatoria.dto';  // Asegúrate de importar este DTO si lo tienes
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,28 @@ export class EmpresaService {
 
   constructor(private http: HttpClient) {}
 
+  // Obtener datos de una empresa
   obtenerEmpresa(id: number): Observable<EmpresaDTO> {
     return this.http.get<EmpresaDTO>(`${this.baseUrl}/${id}`);
   }
 
+  // Actualizar los datos de la empresa
   actualizarEmpresa(id: number, empresa: EmpresaDTO): Observable<any> {
     return this.http.patch(`${this.baseUrl}/${id}`, empresa);
+  }
+
+  // Obtener las convocatorias de la empresa
+  obtenerConvocatorias(id: number): Observable<ConvocatoriaForTableDTO[]> {
+    return this.http.get<ConvocatoriaForTableDTO[]>(`${this.baseUrl}/${id}/convocatorias`);
+  }
+
+  // Obtener convocatorias vigentes de la empresa
+  obtenerConvocatoriasVigentes(id: number, esVigente: boolean): Observable<ConvocatoriaForTableDTO[]> {
+    return this.http.get<ConvocatoriaForTableDTO[]>(`${this.baseUrl}/${id}/convocatoriasVigentes?esVigente=${esVigente}`);
+  }
+
+  // Realizar login de la empresa
+  loginEmpresa(empresa: EmpresaDTO): Observable<number> {
+    return this.http.post<number>(`${this.baseUrl}/login`, empresa);
   }
 }
