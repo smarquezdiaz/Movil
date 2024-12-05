52f4ff8a3868972949ac4f2f011635596bb41379
jhosyS <52686073+jhosyS@users.noreply.github.com>
2024-12-04 23:03:21 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,18 +29,14 @@ const routes: Routes = [
     path: 'perfil-postulante',
     loadChildren: () => import('./pages/perfil-postulante/perfil-postulante.module').then( m => m.PerfilPostulantePageModule)
   },
- /* {
+  
+  {
     path: 'perfil-empresa/:id',  
-<<<<<<< HEAD
-    loadChildren: () => import('./pages/perfil-empresa/perfil-empresa.module').then(m => m.PerfilEmpresaPageModule)
-  },  */
-=======
     loadChildren: () => 
       import('./pages/perfil-empresa/perfil-empresa.module').then(
         (m) => m.PerfilEmpresaPageModule
       )
   }
->>>>>>> Jhosy
 
 ];
 
