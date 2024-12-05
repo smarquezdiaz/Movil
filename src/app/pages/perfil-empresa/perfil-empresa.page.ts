9f036c46a10190e837d7c662314d82ba349d9b15
jhosyS <52686073+jhosyS@users.noreply.github.com>
2024-12-05 00:58:17 -0400

    Update perfil-empresa.page.ts

--- src/app/pages/perfil-empresa/perfil-empresa.page.ts
+++ src/app/pages/perfil-empresa/perfil-empresa.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EmpresaService } from '../../services/empresa.service';
-import { Empresa } from '../../modelos/empresa';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ConvocatoriaService } from '../../services/convocatoria.service';
+import { EmpresaDTO } from '../../models/empresa.dto';
+import { ConvocatoriaForTableDTO } from '../../models/convocatoria.dto';
 
 @Component({
   selector: 'app-perfil-empresa',
@@ -9,77 +10,29 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./perfil-empresa.page.scss'],
 })
 export class PerfilEmpresaPage implements OnInit {
-  empresa: Empresa | null = null;
-  idEmpresa: number | null = null;
-  loadingEmpresa: boolean = false;
-  loadingConvocatorias: boolean = false;
-  errorMessageEmpresa: string = '';
-  errorMessageConvocatorias: string = '';
-  convocatoriasPasadas: any[] = [];
+  empresa: EmpresaDTO | null = null;
+  convocatorias: ConvocatoriaForTableDTO[] = [];
 
   constructor(
     private empresaService: EmpresaService,
-    private activatedRoute: ActivatedRoute,
-    private router: Router
+    private convocatoriaService: ConvocatoriaService
   ) {}
 
   ngOnInit() {
-    const idParam = this.activatedRoute.snapshot.paramMap.get('id');
-    this.idEmpresa = idParam ? +idParam : null;
-
-    if (!this.idEmpresa) {
-      this.router.navigate(['/home']);
-      return;
-    }
-
-    this.loadEmpresa();
-    this.loadConvocatoriasPasadas();
+    const empresaId = 1; 
+    this.obtenerPerfilEmpresa(empresaId);
+    this.obtenerConvocatorias(empresaId);
   }
 
-  loadEmpresa() {
-    this.loadingEmpresa = true;
-    this.errorMessageEmpresa = '';
-    this.empresaService.getEmpresa(this.idEmpresa!).subscribe({
-      next: (data) => {
-        this.empresa = data;
-        this.loadingEmpresa = false;
-      },
-      error: (err) => {
-        console.error('Error al cargar la empresa:', err);
-        this.errorMessageEmpresa = 'No se pudo cargar la información de la empresa.';
-        this.loadingEmpresa = false;
-      },
+  obtenerPerfilEmpresa(id: number) {
+    this.empresaService.obtenerEmpresa(id).subscribe((empresa) => {
+      this.empresa = empresa;
     });
   }
 
-  saveChanges() {
-    if (this.empresa && this.idEmpresa) {
-      this.errorMessageEmpresa = '';
-      this.empresaService.updateEmpresa(this.idEmpresa, this.empresa).subscribe({
-        next: () => {
-          console.log('Cambios guardados exitosamente');
-        },
-        error: (err) => {
-          console.error('Error al guardar los cambios:', err);
-          this.errorMessageEmpresa = 'No se pudieron guardar los cambios. Intenta nuevamente.';
-        },
-      });
-    }
-  }
-
-  loadConvocatoriasPasadas() {
-    this.loadingConvocatorias = true;
-    this.errorMessageConvocatorias = '';
-    this.empresaService.getConvocatorias(this.idEmpresa!).subscribe({
-      next: (data) => {
-        this.convocatoriasPasadas = data.filter((convocatoria) => new Date(convocatoria.fechaFin) < new Date());
-        this.loadingConvocatorias = false;
-      },
-      error: (err) => {
-        console.error('Error al cargar las convocatorias pasadas:', err);
-        this.errorMessageConvocatorias = 'No se pudieron cargar las convocatorias.';
-        this.loadingConvocatorias = false;
-      },
+  obtenerConvocatorias(id: number) {
+    this.convocatoriaService.obtenerConvocatoriasDeEmpresa(id).subscribe((convocatorias) => {
+      this.convocatorias = convocatorias;
     });
   }
 }
