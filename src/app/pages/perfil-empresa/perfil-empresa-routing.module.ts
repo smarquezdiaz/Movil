1cf377634fb5cbee5f8dfd556d788ec73d9bc761
jhosyS <52686073+jhosyS@users.noreply.github.com>
2024-12-05 01:23:42 -0400

    Create perfil-empresa-routing.module.ts

---
+++ src/app/pages/perfil-empresa/perfil-empresa-routing.module.ts
@@ -0,0 +1,17 @@
+// perfil-empresa-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PerfilEmpresaPage } from './perfil-empresa.page'; // Asegúrate de importar correctamente tu página
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PerfilEmpresaPage
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PerfilEmpresaRoutingModule {}
