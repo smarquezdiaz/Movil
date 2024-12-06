ddabdc42a129371cd5ad932775734fcfb2ff5951
smarquezdiaz <solabril2015@hotmail.com>
2024-12-05 23:00:38 -0400

    backend deployado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:8080',
+  api: 'http://5.78.70.226:8081',
+  // api: 'http://localhost:8080', En caso de querer usar local 
   backend: {
     user: '/user',
     empresa:  '/empresas',
