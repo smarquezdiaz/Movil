dedea476a72dd25f4479281fd336938f0094c361
alan jesus caceres medrano <caceresalan2474@gmail.com>
2024-12-07 16:57:58 -0400

    config trafic http

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'AppMobile',
-  webDir: 'www'
+  webDir: 'www',
+  "server": {
+    "cleartext": true
+  }
 };
 
 export default config;
