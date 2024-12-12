23f500a24a8608a108e7ff4b4c256217290aba2d
jhosyS <52686073+jhosyS@users.noreply.github.com>
2024-12-12 01:11:37 -0400

    Update empresa.service.ts

--- src/app/services/empresa.service.ts
+++ src/app/services/empresa.service.ts
@@ -3,32 +3,31 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EmpresaDTO } from '../_DTO/empresaDTO';
 import { ConvocatoriaForTableDTO } from '../_DTO/convocatoriaForTableDTO';
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpresaService {
-  private baseUrl = 'http://localhost:8080/empresas';  // Cambia esto por tu URL base
+  private baseUrl = environment.api + environment.backend.empresa; 
   
   constructor(private http: HttpClient) {}
 
-  createEmpresa(empresaDTO: EmpresaDTO){
+  createEmpresa(empresaDTO: EmpresaDTO): Observable<EmpresaDTO> {
     return this.http.post<EmpresaDTO>(`${this.baseUrl}`, empresaDTO);
   }
 
-  getConvocatorias(idEmpresa: number){
+  getConvocatorias(idEmpresa: number): Observable<ConvocatoriaForTableDTO[]> {
     return this.http.get<ConvocatoriaForTableDTO[]>(`${this.baseUrl}/${idEmpresa}/convocatorias`);
   }
 
-  getConvocatoriasVigentes(idEmpresa: number , esVigente : boolean){
-    return this.http.get<ConvocatoriaForTableDTO[]>(`${this.baseUrl}/${idEmpresa}/convocatorias`);
+  getConvocatoriasVigentes(idEmpresa: number, esVigente: boolean): Observable<ConvocatoriaForTableDTO[]> {
+    return this.http.get<ConvocatoriaForTableDTO[]>(`${this.baseUrl}/${idEmpresa}/convocatoriasVigentes?esVigente=${esVigente}`);
   }
 
   // Realizar login de la empresa
   loginEmpresa(empresa: EmpresaDTO): Observable<number> {
     return this.http.post<number>(`${this.baseUrl}/login`, empresa);
- 
   }
 
   // Obtener datos de una empresa
@@ -38,36 +37,6 @@ export class EmpresaService {
 
   // Actualizar los datos de la empresa
   updateEmpresa(idEmpresa: number, empresaDTO: EmpresaDTO): Observable<any> {
-    return this.http.patch(`${this.baseUrl}/${idEmpresa}`, empresaDTO);
+    return this.http.put(`${this.baseUrl}/${idEmpresa}`, empresaDTO);
   }
-  
-  /*
- 
-  // Obtener las convocatorias de la empresa
-  obtenerConvocatorias(id: number): Observable< private Integer id;[]> {
-    return this.http.get<ConvocatoriaForTableDTO[]>(`${this.baseUrl}/${id}/convocatorias`);
-  }
-
-  // Obtener convocatorias vigentes de la empresa
-  obtenerConvocatoriasVigentes(id: number, esVigente: boolean): Observable<ConvocatoriaForTableDTO[]> {
-    return this.http.get<ConvocatoriaForTableDTO[]>(`${this.baseUrl}/${id}/convocatoriasVigentes?esVigente=${esVigente}`);
-  }
-
-  // Obtener las convocatorias por empresa (Método adicional)
-  obtenerConvocatoriasPorEmpresa(idEmpresa: number): Observable<ConvocatoriaForTableDTO[]> {
-    return this.http.get<ConvocatoriaForTableDTO[]>(`${this.baseUrl}/${idEmpresa}/convocatorias`);
-  }
-
-  // Obtener convocatorias filtradas de la empresa (Método adicional)
-  obtenerConvocatoriasFiltradas(idEmpresa: number, activo: boolean): Observable<ConvocatoriaForTableDTO[]> {
-    return this.http.get<ConvocatoriaForTableDTO[]>(`${this.baseUrl}/${idEmpresa}/convocatoriasFiltradas?activo=${activo}`);
-  }
-
-  // Realizar login de la empresa
-  loginEmpresa(empresa: EmpresaDTO): Observable<number> {
-    return this.http.post<number>(`${this.baseUrl}/login`, empresa);
- 
-  }
-*/
-
 }
