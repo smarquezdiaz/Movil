be473e6d3b5bec19b0602f67e254154376191e95
jhosyS <52686073+jhosyS@users.noreply.github.com>
2024-12-12 02:47:28 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   api: 'http://5.78.70.226:8081',
   // api: 'http://localhost:8080', En caso de querer usar local 
   backend: {
@@ -10,14 +9,5 @@ export const environment = {
     imagen: '/imagen',
     postulante: '/postulantes',
     email: '/send-email'
-=======
-  api: 'http://localhost:8080',  
-  backend: {
-    user: '/user',             
-    empresa: '/empresas',        
-    convocatoria: '/convocatorias',  
-    imagen: '/imagen',           
-    postulante: '/postulantes'   
->>>>>>> origin/Jhosy
   }
 };
